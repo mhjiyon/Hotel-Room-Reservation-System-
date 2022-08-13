4a987920ef67f933bd65132d065c0dedb0796505
mhjiyon <107164054+mhjiyon@users.noreply.github.com>
2022-08-13 20:25:40 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel North South-contact</title>
+    <?php require('links.php'); ?>
+
+</head>
+<body class="bg=light">
+
+ <!-- Navigation -->   
+<?php require('header.php'); ?>
+
+<div class="my-5 px-4">
+   <h2 class="fw-bold  text-center">CONTACT US</h2>
+   <div class="h-line bg-dark"></div>
+   <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Nobis sequi obcaecati <br> dolor facere iusto provident similique 
+    totam quo laborum aspernatur.
+   </p>
+</div>
+
+<div class="container">
+  <div class="row">
+      <div class="col-lg-6 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 ">
+        <iframe class="w-100 rounded mb-4" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.097953892908!2d90.42336791498151!3d23.815115592217882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64c103a8093%3A0xd660a4f50365294a!2sNorth%20South%20University!5e0!3m2!1sen!2sbd!4v1660143682084!5m2!1sen!2sbd" loading="lazy"></iframe>
+        <h5 class="h-font fw-bold"></i> Address</h5>
+        <a href="https://goo.gl/maps/HV5i5Vr7xAr7zx579" target="blank" class="d-inline-block text-decoration-none text-dark mb-2">
+        <i class="bi bi-geo-alt-fill"></i>Boshundhara,Block-C,Dhaka,Bangladesh</a>
+        <h5 class="mt-2 h-font fw-bold"></i>Contact Us</h5>
+                <a href="tel: +8801873511509" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-telephone-fill"></i>+8801873511509
+                </a>
+                <br>
+                <a href="tel: +8801873511509" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-telephone-fill"></i>+8801985771329
+                </a>
+         <h5 class="mt-2 h-font">Email</h5> 
+                <a href="mailto: hasan.jiyon@northsouth.edu" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-envelope-fill"></i> hotelnorthsouth@gmail.com
+                </a>
+
+                <h5 class="mt-2">Follow Us</h5>
+
+                <a href="https://twitter.com/NorthSouthU" target="blank" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                    <i class="bi bi-twitter me-1"  ></i></a>
+                     
+                
+                <a href="https://www.facebook.com/NorthSouthUniversity" target="blank" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                        <i class="bi bi-facebook me-1"></i></a>
+               
+               <a href="https://www.instagram.com/northsouthuniversity" target="blank" class="d-inline-block mb-3 text-dark fs-5 me-2">
+                   <i class="bi bi-instagram "></i></a>
+      </div>
+  </div>
+
+      <div class="col-lg-6 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 ">
+          <form >
+                <h5>Send a message</h5>
+                <div class="mt-3">
+                    <label  class="form-label" style="font-weight:500;">Name</label>
+                      <input type="text" class="form-control shadow-none">
+                  </div>
+                  <div class="mt-3">
+                    <label  class="form-label" style="font-weight:500;">Email</label>
+                      <input type="email" class="form-control shadow-none">
+                  </div>
+                  <div class="mt-3">
+                    <label  class="form-label" style="font-weight:500;">Subject</label>
+                      <input type="text" class="form-control shadow-none">
+                  </div>
+                  <div class="mt-3">
+                    <label  class="form-label" style="font-weight:500;">Message</label>
+                    <textarea class="form-control"rows="7" style="resize:none"></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-white custom-bg mt-3">SUBMIT</button>
+          </form>
+        
+        </div>
+      </div>
+    
+
+  </div>
+</div>
+
+    <!-- footer -->
+<?php require('footer.php'); ?>
+
+</body>
+</html>
